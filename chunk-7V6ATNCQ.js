88d4eb80768b38ccb3a5dcea01ba84798e0721b7
Saeedtaia <Saeedtaia@users.noreply.github.com>
2025-06-02 12:20:06 +0000

    deploy: 634ff46ddf8df00a5190b8cf9ade9b2b7e9ea2cc

---
+++ chunk-7V6ATNCQ.js
@@ -0,0 +1,1 @@
+var t={baseUrl:"https://saeedtaia.github.io/SAKAN/",production:!0,apiUrl:"http://housing-sys.runasp.net/api/v1/",ImageUrl:"http://housing-sys.runasp.net/",debugMode:!1};export{t as a};
